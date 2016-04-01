c2b2572574f83189db7b87dfa3f618a4ef6ef070
qinchuanlong <1270253618@qq.com>
2016-04-01 15:13:56 +0800

    adsf

---
+++ gitDemo/gitDemo/Preso.h
@@ -0,0 +1,13 @@
+//
+//  Preso.h
+//  gitDemo
+//
+//  Created by qianfeng on 16/4/1.
+//  Copyright © 2016年 秦传龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Preso : UIView
+
+@end
---
+++ gitDemo/gitDemo/Preso.m
@@ -0,0 +1,21 @@
+//
+//  Preso.m
+//  gitDemo
+//
+//  Created by qianfeng on 16/4/1.
+//  Copyright © 2016年 秦传龙. All rights reserved.
+//
+
+#import "Preso.h"
+
+@implementation Preso
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
