9c3efa8b76f9c10259c25604fb6b95c7bca46d65
qinchuanlong <1270253618@qq.com>
2016-04-01 15:11:55 +0800

    asdf

---
+++ gitDemo/gitDemo/PersonB.h
@@ -0,0 +1,13 @@
+//
+//  PersonB.h
+//  gitDemo
+//
+//  Created by qianfeng on 16/4/1.
+//  Copyright © 2016年 秦传龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PersonB : UIView
+
+@end
---
+++ gitDemo/gitDemo/PersonB.m
@@ -0,0 +1,21 @@
+//
+//  PersonB.m
+//  gitDemo
+//
+//  Created by qianfeng on 16/4/1.
+//  Copyright © 2016年 秦传龙. All rights reserved.
+//
+
+#import "PersonB.h"
+
+@implementation PersonB
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
